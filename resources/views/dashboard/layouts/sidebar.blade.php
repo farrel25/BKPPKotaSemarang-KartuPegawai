9412eb67a83e02a5594c26c1f4dd6609ef1742bd
farrel25 <frl25ap@gmail.com>
2021-01-29 06:05:27 +0700

    fixing dashboard sidebar menu

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -33,11 +33,69 @@
     <div class="scrollbar-sidebar">
         <div class="app-sidebar__inner">
             <ul class="vertical-nav-menu">
-                <li class="app-sidebar__heading">Dashboards</li>
+                <li class="app-sidebar__heading">Menu Utama</li>
                 <li>
                     <a href="index.html" class="mm-active">
                         <i class="metismenu-icon pe-7s-rocket"></i>
-                        Dashboard Example 1
+                        Dashboard
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="metismenu-icon pe-7s-rocket"></i>
+                        Kembali ke Beranda
+                    </a>
+                </li>
+                <li class="app-sidebar__heading">Admin</li>
+                <li>
+                    <a href="#">
+                        <i class="metismenu-icon pe-7s-rocket"></i>
+                        Pengajuan Kartu Pegawai
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="metismenu-icon pe-7s-rocket"></i>
+                        Kartu dicetak
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="metismenu-icon pe-7s-rocket"></i>
+                        Kartu diambil
+                    </a>
+                </li>
+                <li class="app-sidebar__heading">Manajemen Menu</li>
+                <li>
+                    <a href="#">
+                        <i class="metismenu-icon pe-7s-rocket"></i>
+                        Dashboard Menu
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="metismenu-icon pe-7s-rocket"></i>
+                        Dashboard Sub Menu
+                    </a>
+                </li>
+                <li class="app-sidebar__heading">Manajemen Pengguna</li>
+                <li>
+                    <a href="#">
+                        <i class="metismenu-icon pe-7s-rocket"></i>
+                        Akun Pengguna
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="metismenu-icon pe-7s-rocket"></i>
+                        Role dan Hak Akses
+                    </a>
+                </li>
+                <li class="app-sidebar__heading">Pegawai</li>
+                <li>
+                    <a href="#">
+                        <i class="metismenu-icon pe-7s-rocket"></i>
+                        Pengajuan Kartu Pegawai
                     </a>
                 </li>
                 <li class="app-sidebar__heading">UI Components</li>
@@ -90,115 +148,6 @@
                             </a>
                         </li>
                     </ul>
-                </li>
-                <li>
-                    <a href="#">
-                        <i class="metismenu-icon pe-7s-car"></i>
-                        Components
-                        <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
-                    </a>
-                    <ul>
-                        <li>
-                            <a href="components-tabs.html">
-                                <i class="metismenu-icon"> </i>Tabs
-                            </a>
-                        </li>
-                        <li>
-                            <a href="components-accordions.html">
-                                <i class="metismenu-icon"> </i>Accordions
-                            </a>
-                        </li>
-                        <li>
-                            <a href="components-notifications.html">
-                                <i class="metismenu-icon"> </i>Notifications
-                            </a>
-                        </li>
-                        <li>
-                            <a href="components-modals.html">
-                                <i class="metismenu-icon"> </i>Modals
-                            </a>
-                        </li>
-                        <li>
-                            <a href="components-progress-bar.html">
-                                <i class="metismenu-icon"> </i>Progress Bar
-                            </a>
-                        </li>
-                        <li>
-                            <a href="components-tooltips-popovers.html">
-                                <i class="metismenu-icon"> </i>Tooltips &amp;
-                                Popovers
-                            </a>
-                        </li>
-                        <li>
-                            <a href="components-carousel.html">
-                                <i class="metismenu-icon"> </i>Carousel
-                            </a>
-                        </li>
-                        <li>
-                            <a href="components-calendar.html">
-                                <i class="metismenu-icon"> </i>Calendar
-                            </a>
-                        </li>
-                        <li>
-                            <a href="components-pagination.html">
-                                <i class="metismenu-icon"> </i>Pagination
-                            </a>
-                        </li>
-                        <li>
-                            <a href="components-scrollable-elements.html">
-                                <i class="metismenu-icon"> </i>Scrollable
-                            </a>
-                        </li>
-                        <li>
-                            <a href="components-maps.html">
-                                <i class="metismenu-icon"> </i>Maps
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="tables-regular.html">
-                        <i class="metismenu-icon pe-7s-display2"></i>
-                        Tables
-                    </a>
-                </li>
-                <li class="app-sidebar__heading">Widgets</li>
-                <li>
-                    <a href="dashboard-boxes.html">
-                        <i class="metismenu-icon pe-7s-display2"></i>
-                        Dashboard Boxes
-                    </a>
-                </li>
-                <li class="app-sidebar__heading">Forms</li>
-                <li>
-                    <a href="forms-controls.html">
-                        <i class="metismenu-icon pe-7s-mouse"> </i>Forms Controls
-                    </a>
-                </li>
-                <li>
-                    <a href="forms-layouts.html">
-                        <i class="metismenu-icon pe-7s-eyedropper"> </i>Forms
-                        Layouts
-                    </a>
-                </li>
-                <li>
-                    <a href="forms-validation.html">
-                        <i class="metismenu-icon pe-7s-pendrive"> </i>Forms
-                        Validation
-                    </a>
-                </li>
-                <li class="app-sidebar__heading">Charts</li>
-                <li>
-                    <a href="charts-chartjs.html">
-                        <i class="metismenu-icon pe-7s-graph2"> </i>ChartJS
-                    </a>
-                </li>
-                <li class="app-sidebar__heading">PRO Version</li>
-                <li>
-                    <a href="https://dashboardpack.com/theme-details/architectui-dashboard-html-pro/" target="_blank">
-                        <i class="metismenu-icon pe-7s-graph2"> </i>
-                        Upgrade to PRO
-                    </a>
                 </li>
             </ul>
         </div>
