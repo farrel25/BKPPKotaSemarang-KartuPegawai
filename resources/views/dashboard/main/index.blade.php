5bba739b05a989b57150e82bd7ae7d6268c0b81c
babhistta <babhistta@gmail.com>
2021-02-05 15:49:41 +0700

    Update index.blade.php

--- resources/views/dashboard/main/index.blade.php
+++ resources/views/dashboard/main/index.blade.php
@@ -17,8 +17,8 @@
         <div class="card mb-3 widget-content bg-grow-early">
             <div class="widget-content-wrapper text-white">
                 <div class="widget-content-left">
-                    <div class="widget-heading">Surat Masuk</div>
-                    <div class="widget-subheading">Total Surat Masuk</div>
+                    <div class="widget-heading">Pegawai</div>
+                    <div class="widget-subheading">Total Pegawai</div>
                 </div>
                 <div class="widget-content-right">
                     <div class="widget-numbers text-white"><span>100</span></div>
@@ -30,8 +30,8 @@
         <div class="card mb-3 widget-content bg-plum-plate  ">
             <div class="widget-content-wrapper text-white">
                 <div class="widget-content-left">
-                    <div class="widget-heading">Surat Keluar</div>
-                    <div class="widget-subheading">Total Surat Keluar</div>
+                    <div class="widget-heading">Pengguna</div>
+                    <div class="widget-subheading">Total Pengguna</div>
                 </div>
                 <div class="widget-content-right">
                     <div class="widget-numbers text-white"><span>100</span></div>
@@ -43,8 +43,8 @@
         <div class="card mb-3 widget-content bg-strong-bliss   ">
             <div class="widget-content-wrapper text-white">
                 <div class="widget-content-left">
-                    <div class="widget-heading">Pengajuan Surat</div>
-                    <div class="widget-subheading">Total Pengajuan Surat</div>
+                    <div class="widget-heading">Pengajuan </div>
+                    <div class="widget-subheading">Total Pengajuan Kartu</div>
                 </div>
                 <div class="widget-content-right">
                     <div class="widget-numbers text-white"><span>100</span></div>
@@ -53,7 +53,7 @@
         </div>
     </div>
 </div>
-<div class="row">
+{{-- <div class="row">
     <div class="col-md-12">
         <div class="main-card mb-3 card">
             <div class="no-gutters row">
@@ -100,7 +100,7 @@
         </div>
     </div>
 
-</div>
+</div> --}}
 <div class="row">
     <div class="col-md-12 col-lg-12">
         <div class="mb-3 card">
@@ -120,8 +120,9 @@
                                             <div class="widget-content-outer">
                                                 <div class="widget-content-wrapper">
                                                     <div class="widget-content-left">
-                                                        <div class="widget-heading">Penduduk</div>
-                                                        <div class="widget-subheading">Total Seluruh Penduduk </div>
+                                                        <div class="widget-heading">Kartu Pegawai</div>
+                                                        <div class="widget-subheading">Jumlah Pengajuan + Diproses +
+                                                            Pengambilan + Selesai </div>
                                                     </div>
                                                     <div class="widget-content-right">
                                                         <div class="widget-numbers text-danger">100</div>
@@ -142,18 +143,19 @@
                                             <div class="widget-content-outer">
                                                 <div class="widget-content-wrapper">
                                                     <div class="widget-content-left">
-                                                        <div class="widget-heading">Aktif</div>
-                                                        <div class="widget-subheading">Total Penduduk Aktif</div>
+                                                        <div class="widget-heading">Belum Diproses</div>
+                                                        <div class="widget-subheading">Total Kartu Belum Diproses</div>
                                                     </div>
                                                     <div class="widget-content-right">
-                                                        <div class="widget-numbers text-success">75</div>
+                                                        <div class="widget-numbers text-secondary">10</div>
                                                     </div>
                                                 </div>
                                                 <div class="widget-progress-wrapper">
                                                     <div class="progress-bar-lg progress-bar-animated progress">
-                                                        <div class="progress-bar progress-bar-animated bg-success progress-bar-striped"
-                                                            role="progressbar" aria-valuenow="75" aria-valuemin="0"
-                                                            aria-valuemax="100" style="width: 75%;">75%</div>
+                                                        <div class="progress-bar progress-bar-animated bg-secondary progress-bar-striped"
+                                                            role="progressbar" aria-valuenow="10" aria-valuemin="0"
+                                                            aria-valuemax="100" style="width: 10%;">10%
+                                                        </div>
                                                     </div>
                                                     <div class="progress-sub-label">
                                                         <div class="sub-label-left">presentase</div>
@@ -168,19 +170,72 @@
                                             <div class="widget-content-outer">
                                                 <div class="widget-content-wrapper">
                                                     <div class="widget-content-left">
-                                                        <div class="widget-heading">Tidak Aktif</div>
-                                                        <div class="widget-subheading">Total Penduduk Tidak Aktif</div>
+                                                        <div class="widget-heading">Diproses</div>
+                                                        <div class="widget-subheading">Total Kartu Dalam Pencetakan
+                                                        </div>
                                                     </div>
                                                     <div class="widget-content-right">
-                                                        <div class="widget-numbers text-warning">25</div>
+                                                        <div class="widget-numbers text-alternate">10</div>
+                                                    </div>
+                                                </div>
+                                                <div class="widget-progress-wrapper">
+                                                    <div class="progress-bar-lg progress-bar-animated progress">
+                                                        <div class="progress-bar progress-bar-animated bg-alternate progress-bar-striped"
+                                                            role="progressbar" aria-valuenow="10" aria-valuemin="0"
+                                                            aria-valuemax="100" style="width: 10%;">10%</div>
+                                                    </div>
+                                                    <div class="progress-sub-label">
+                                                        <div class="sub-label-left">presentase</div>
+                                                        <div class="sub-label-right">100%</div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 ">
+                                        <div class=" mb-3 widget-content">
+                                            <div class="widget-content-outer">
+                                                <div class="widget-content-wrapper">
+                                                    <div class="widget-content-left">
+                                                        <div class="widget-heading">Pengambilan</div>
+                                                        <div class="widget-subheading">Total Kartu Belum Diambil</div>
+                                                    </div>
+                                                    <div class="widget-content-right">
+                                                        <div class="widget-numbers text-warning">10</div>
                                                     </div>
                                                 </div>
                                                 <div class="widget-progress-wrapper">
                                                     <div class="progress-bar-lg progress-bar-animated progress">
                                                         <div class="progress-bar progress-bar-animated bg-warning progress-bar-striped"
-                                                            role="progressbar" aria-valuenow="25" aria-valuemin="0"
-                                                            aria-valuemax="100" style="width: 25%;">25%
+                                                            role="progressbar" aria-valuenow="10" aria-valuemin="0"
+                                                            aria-valuemax="100" style="width: 10%;">10%
                                                         </div>
+                                                    </div>
+                                                    <div class="progress-sub-label">
+                                                        <div class="sub-label-left">presentase</div>
+                                                        <div class="sub-label-right">100%</div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 ">
+                                        <div class=" mb-3 widget-content">
+                                            <div class="widget-content-outer">
+                                                <div class="widget-content-wrapper">
+                                                    <div class="widget-content-left">
+                                                        <div class="widget-heading">Selesai</div>
+                                                        <div class="widget-subheading">Total Kartu Sudah DIambil</div>
+                                                    </div>
+                                                    <div class="widget-content-right">
+                                                        <div class="widget-numbers text-success">70</div>
+                                                    </div>
+                                                </div>
+                                                <div class="widget-progress-wrapper">
+                                                    <div class="progress-bar-lg progress-bar-animated progress">
+                                                        <div class="progress-bar progress-bar-animated bg-success progress-bar-striped"
+                                                            role="progressbar" aria-valuenow="70" aria-valuemin="0"
+                                                            aria-valuemax="100" style="width: 70%;">70%</div>
                                                     </div>
                                                     <div class="progress-sub-label">
                                                         <div class="sub-label-left">presentase</div>
